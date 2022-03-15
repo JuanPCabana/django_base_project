32e29ae7d37b974437a2cc406751264a69d7471f
Juan Perez <juanpc3399@gmail.com>
2022-03-15 15:51:40 -0400

    base backend application without a complex frontend

--- votaciones/polls/urls.py
+++ votaciones/polls/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 
 from . import views
 
-
+app_name = "polls"
 urlpatterns = [
-              path("", views.index, name="index")
+              #ex: /polls/
+              path("", views.IndexView.as_view(), name="index"),
+              #ex: /polls/2/  
+              path("<int:question_id>/", views.detail, name="detail"),
+              #ex: /polls/2/results/ 
+              path("<int:question_id>/results/", views.results, name="results"),
+              #ex: /polls/2/vote/ 
+              path("<int:question_id>/vote/", views.vote, name="vote"),
               ]
